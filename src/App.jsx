ceb4e464f10522c30f333870581e0f2ce517cb8e
ARUN-KUMAR-YARLAGADDA <arunkumar_yarlagadda@srmap.edu.in>
2025-05-21 18:05:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='bg-red-700'>Hello!</h1>
+    </div>
+  )
+}
+
+export default App
