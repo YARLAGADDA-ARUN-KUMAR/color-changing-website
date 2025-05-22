579568f86918ce0d28c81c27ca1e64f3ea260beb
ARUN-KUMAR-YARLAGADDA <arunkumar_yarlagadda@srmap.edu.in>
2025-05-22 06:45:42 +0530

    Rainbow colors

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
 
   return (
     <div className='h-screen flex justify-center items-center' style={{backgroundColor: color}}>
-      <div className='inline-block space-x-4 size-fit rounded-4xl p-2 text-xl font-bold'>
-        <button className='inline-block p-1.5 border-3 rounded-3xl bg-violet-800 text-white' onClick={() => setColor('#8F00FF')}>Violet</button>
-        <button className='inline-block p-1.5 border-3 rounded-3xl bg-indigo-800 text-white' onClick={() => setColor('indigo')}>Indigo</button>
-        <button className='inline-block p-1.5 border-3 rounded-3xl bg-blue-800 text-white' onClick={() => setColor('blue')}>Blue</button>
-        <button className='inline-block p-1.5 border-3 rounded-3xl bg-green-800 text-white' onClick={() => setColor('green')}>Green</button>
-        <button className='inline-block p-1.5 border-3 rounded-3xl bg-yellow-800 text-white' onClick={() => setColor('yellow')}>Yellow</button>
-        <button className='inline-block p-1.5 border-3 rounded-3xl bg-orange-800 text-white' onClick={() => setColor('orange')}>Orange</button>
-        <button className='inline-block p-1.5 border-3 rounded-3xl mr-2 bg-red-800 text-white' onClick={() => setColor('red')}>Red</button>
+      <div className='inline-block space-x-4 size-fit rounded-4xl p-2 text-xl font-bold bg-gray-300'>
+        <button className='inline-block p-1.5 border-3 rounded-3xl bg-violet-800 text-white cursor-pointer' onClick={() => setColor('#8F00FF')}>Violet</button>
+        <button className='inline-block p-1.5 border-3 rounded-3xl bg-indigo-800 text-white cursor-pointer' onClick={() => setColor('indigo')}>Indigo</button>
+        <button className='inline-block p-1.5 border-3 rounded-3xl bg-blue-800 text-white cursor-pointer' onClick={() => setColor('blue')}>Blue</button>
+        <button className='inline-block p-1.5 border-3 rounded-3xl bg-green-800 text-white cursor-pointer' onClick={() => setColor('green')}>Green</button>
+        <button className='inline-block p-1.5 border-3 rounded-3xl bg-yellow-800 text-white cursor-pointer' onClick={() => setColor('yellow')}>Yellow</button>
+        <button className='inline-block p-1.5 border-3 rounded-3xl bg-orange-800 text-white cursor-pointer' onClick={() => setColor('orange')}>Orange</button>
+        <button className='inline-block p-1.5 border-3 rounded-3xl mr-2 bg-red-800 text-white cursor-pointer' onClick={() => setColor('red')}>Red</button>
       </div>
     </div>
   )
